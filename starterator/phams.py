ad2d764055676236f8d8ec1dcd8e55cca8d33f48
Chris Shaffer <cdshaffer@gmail.com>
2015-07-04 13:17:45 -0500

    update code to log off by one situations to desktop file

--- starterator/phams.py
+++ starterator/phams.py
@@ -102,9 +102,7 @@ class Pham(object):
             gene.add_gaps_as_features()
 
     def call_clustal(self, fasta_file):
-        subprocess.check_call(['clustalw', 
-        '-infile=%s' % (fasta_file),
-        '-quicktree'])
+        subprocess.check_call(['clustalw',  '-infile=%s' % (fasta_file), '-quicktree'])
 
     def make_fasta(self, file_name=None):
         if file_name == None:
@@ -232,10 +230,13 @@ class Pham(object):
                     gene.suggested_start["most_called"] =(most_called_start_index, gene.start+1)
                     #off by one error on suggested start: error genes get to here and gene.start is correct (no +1 needed)
                     start_stats["most_called"].append(gene.gene_id)
-                    if gene.imputedStart != gene.suggested_start["most_called"][1]:
-                        with open('/home/seastudent/Desktop/offbyone.txt', 'a') as f:
-                            f.write('Gene: ' + str(gene.gene_id) + ' at ' + str(gene.imputedStart) + str(gene.orientation)
-                                    + ' OFF by one ' + str( gene.suggested_start["most_called"][1]) + '\n')
+
+                    with open('/home/seastudent/Desktop/offbyone.txt', 'a') as f:
+                        f.write("F, ")
+                        if gene.orientation == 'R':
+                            f.write('\nGene: ' + str(gene.gene_id) + ' at ' + str(gene.imputedStart) + str(gene.orientation))
+                            if gene.imputedStart != gene.suggested_start["most_called"][1]:
+                                f.write(' OFF by one ' + str( gene.suggested_start["most_called"][1]))
                 else:                                                            #current gene not annotated with most called start
                     start_stats["most_not_called"].append(gene.gene_id)
                     most_called_alignment_index = self.total_possible_starts[most_called_start_index-1]
