fb04fc02bad9c183136f4d7580591a25d2a71242
Chris Shaffer <cdshaffer@gmail.com>
2016-07-08 16:41:26 -0500

    update track name to include number of other members

--- starterator/making_files.py
+++ starterator/making_files.py
@@ -159,8 +159,15 @@ def make_gene_track(gd_diagram, pham, gene_group, num_on_diagram, total):
     """"""
     colors = ['purple', 'red', 'lightblue', 'orange', 'tan', 'brown']
     gene = gene_group[0]
+
+
+    #change trackname to name of fist gene in list
+    track_name = gene.gene_id
+    if len(gene_group) > 1:
+        track_name += " + "
+        track_name += str(len(gene_group)-1)
     gd_gene_track = gd_diagram.new_track(total - num_on_diagram, label=True,
-                                         name=gene.gene_id, greytrack=1)
+                                         name=track_name, greytrack=1)
     gd_seq_set = gd_gene_track.new_set()
     gd_feature_set = gd_gene_track.new_set()
 
